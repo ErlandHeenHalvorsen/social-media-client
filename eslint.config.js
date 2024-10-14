f37c9f32fe163b18eb67bc8ee4a8181c06d184d4
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-10-14 12:14:28 +0200

    Added and configured jest with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import globals from "globals";
+import pluginJest from "eslint-plugin-jest";
+
 export default [
 	js.configs.recommended,
 	{
@@ -15,6 +17,19 @@ export default [
 		},
 		rules: {
 			...prettierConfig.rules,
+		},
+	},
+	{
+		files: ["tests/**/*.js", "src/**/*.test.js", "src/**/*.spec.js"],
+		plugins: {
+			jest: pluginJest,
+		},
+		env: {
+			"jest/globals": true,
+		},
+		extends: ["plugin:jest/recommended"],
+		rules: {
+			...pluginJest.configs.recommended.rules,
 		},
 	},
 ];
